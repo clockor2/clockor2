e40110c65dbcad40b5377baa422531c10d6d6c5d
wytamma <wytamma.wirth@me.com>
2022-09-21 13:28:14 +1000

    :white_check_mark: add test from something

---
+++ src/features/engine/core.test.ts
@@ -0,0 +1,8 @@
+import { something } from "./core";
+
+describe('testing something', () => {
+    test('something should error', () => {
+        const res = something([], [], '')
+        expect(res[0]).toHaveProperty('mode')
+    });
+  });
