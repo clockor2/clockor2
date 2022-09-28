8e036cedc41aea919e063f5e01b1a89dc88426f2
wytamma <wytamma.wirth@me.com>
2022-09-28 11:33:09 +1000

    :art: regression reads data from state

--- src/features/regression/Regression.tsx
+++ src/features/regression/Regression.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import Plotly from "plotly.js";
 import createPlotlyComponent from "react-plotly.js/factory";
+import { selectData } from './regressionSlice';
+import { useAppSelector } from '../../app/hooks';
 const Plot = createPlotlyComponent(Plotly);
 
 
 export function Regression(props: any) {
-  const data = [
-    {
-      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      mode: "lines",
-    },
-  ];
+  const data = useAppSelector(selectData);
   const layout = { width: props.size.width, height: props.size.height };
   return (
     <div>
