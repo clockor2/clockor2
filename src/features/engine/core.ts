14f839a31e6f36778a44a31fc186d3be1f7d27c1
Wytamma Wirth <wytamma.wirth@me.com>
2022-10-13 14:06:14 +1100

    :art: use RegressionData interface in core

--- src/features/engine/core.ts
+++ src/features/engine/core.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line 
 
+import { RegressionData } from "../regression/regressionSlice";
+
 /* TO DO LIST @ 2022-10-10
 - Add if/for flow control to handle groups
 - Add core function for tree clock search
@@ -24,19 +26,12 @@ export const something = (tipHeights: Array<number>, dates: Array<number>) => {
 // BELOW: FUNCTIONS USED INSIDE CORE ENGINE FUNCTIONS //
 ////////////////////////////////////////////////////////
 
-// Define basic plot class, instances of which will form the data part of the plotly command
-class plotData {
-  x: Array<number> = [];
-  y: Array<number> = [];
-  mode: string = "";
-}
-
 
 // function gets base points for whole regression. Later subdivided by `group`
-const basePoints = (tipHeights: Array<number>, dates: Array<number>) => { 
+const basePoints = (tipHeights: Array<number>, dates: Array<number>): RegressionData => { 
   let y = tipHeights; // TODO: find tip type
   let x = dates;
-  const points = new plotData()
+  const points = {} as RegressionData
   points.x = x;
   points.y = y;
   points.mode = "markers";
@@ -45,7 +40,7 @@ const basePoints = (tipHeights: Array<number>, dates: Array<number>) => {
 }
 
 // regression function takes basePoints() output as input
-function linearRegression(points: plotData){
+function linearRegression(points: RegressionData){
   let x = points.x;
   let y = points.y;
 
@@ -69,7 +64,7 @@ function linearRegression(points: plotData){
   let intercept = (sum_y - slope * sum_x)/n;
   let r2 = Math.pow((n*sum_xy - sum_x*sum_y)/Math.sqrt((n*sum_xx-sum_x*sum_x)*(n*sum_yy-sum_y*sum_y)),2);
 
-  const lr = new plotData();
+  const lr = {} as RegressionData
   lr.x = [x.sort((a, b) => a - b)[0], x.sort((a, b) => a - b)[x.length - 1]];
   lr.y = [
     x.sort((a, b) => a - b)[0] * slope + intercept,
