0c7f7794f5095dd3970790246df92a2412bf0304
LeoFeatherstone <leofeatherstone@gmail.com>
2023-01-25 15:25:57 +1100

    added tests and debugged BIC()

--- src/features/engine/core.test.ts
+++ src/features/engine/core.test.ts
@@ -1,8 +1,102 @@
-import { regression } from "./core";
+import * as core from "./core";
+const phylotree = require("phylotree");
 
-describe('testing something', () => {
-    test('something should error', () => {
-        const res = regression([], [], [], [])
-        expect(res).toHaveProperty('fitY')
-    });
+// this one, i believe, is supposed to fail. It does, fortunately.
+// TODO: make a meaningful test here
+// describe('testing something', () => {
+//   test('something should error', () => {
+//     const res = core.regression([], [], [], [])
+//     expect(res).toHaveProperty('fitY')
+//   });
+// });
+
+// TODO (for now)
+// test normal density
+describe('testing normalDensity function', () => {
+  test('density value should match', () => {
+    var d = core.normalDensity(0, 0, 1)
+    expect(d).toEqual(1 / (Math.sqrt(2 * Math.PI)))
   });
+});
+
+// test based on lm(y~x) in R, where x = 0:5, y = c(1, 0, 3, 2, 5, 4) 
+var dummyX = [0, 1, 2, 3, 4, 5];
+var dummyY = [1, 0, 3, 2, 5, 4];
+var dummyTip = dummyX.map(e => `t${e}`);
+var m = core.linearRegression({ x: dummyX, y: dummyY, tip: dummyTip });
+
+// test regresion: R2, slope, intercept, logLik (critical)
+describe('testing linearRegression() function on dummy data', () => {
+  test('density value should match', () => {
+    expect(m.r2).toBeCloseTo(0.6865, 4);
+    expect(m.intercept).toBeCloseTo(0.4286, 4);
+    expect(m.slope).toBeCloseTo(0.8286, 4);
+    expect(m.logLik).toBeCloseTo(-8.244795, 6);
+  });
+});
+
+// test information criterion values
+describe('testing information criteria', () => {
+  test('density value should match', () => {
+    expect(core.AIC([m])).toBeCloseTo(22.48959, 5);
+    expect(core.AICc([m])).toBeCloseTo(34.48959, 5);
+    expect(core.BIC([m])).toBeCloseTo(21.86487, 5);
+  });
+});
+
+// test combination generating socially 
+describe('testing combination generator', () => {
+  test('Want all subsets of [1, 2, 3] of length 2', () => {
+    expect(
+      core.combn([1, 2, 3], 2).sort()
+      ).toEqual(
+        [ [1, 2], [1, 3], [2, 3] ].sort()
+        );
+  });
+});
+
+describe('testing combination generator edge case', () => {
+  test('Want all subsets of ["a"] of length 2', () => {
+    expect(
+      core.combn(['a'], 1).sort()
+      ).toEqual(
+        [ ['a'] ].sort()
+        );
+  });
+});
+
+describe('testing getUnique()', () => {
+  test('Should filter each element to unique values among array', () => {
+    var x = core.getUnique([ ['A', 'B', 'C', 'D'], ['B', 'C', 'D'], ['D'] ]).sort();
+    console.log(x);
+    expect(
+      core.getUnique([ ['A', 'B', 'C', 'D'], ['B', 'C', 'D'], ['D'] ]).sort()
+      ).toEqual(
+        [ ['A'], ['B', 'C'], ['D'] ].sort()
+        );
+  });
+});
+
+describe('testing tree clade generator', () => {
+  test('Want all groups of at least 2 tips with max number 4', () => {
+    // testing functions related to clock search now
+    var nwk = '((A:1, B:1):1,((C:1, D:1):1,E:1):1);';
+    var tree = new phylotree.phylotree(nwk);
+    var expectedGrps = [
+      [ [ 'A', 'B'], [ 'C', 'D', 'E' ] ],
+      [ [ 'C', 'D', 'E' ], [ 'A', 'B' ] ],
+      [ [ 'A', 'B', 'E' ], [ 'C', 'D' ] ]
+    ]
+    // testing 4 clocks max - more than possible but testing bahviour
+    var grps = core.getGroups(tree, 2, 2)
+    console.log(grps)
+    console.log(expectedGrps)
+    expect(
+      grps.sort()
+      ).toEqual(
+        expectedGrps.sort()
+      )
+  })
+})
+
+//TODO: Test edge case where more clocks than possible asked for in clock search
--- src/features/engine/core.ts
+++ src/features/engine/core.ts
@@ -156,6 +156,7 @@ export const regression = (
 
 //   // generate all possibilities for groupings
 //   let allGroups = getGroups(tree, minCladeSize, numClocks);
+//   let allGroupsNumber = allGroups.map((e: string[][]) => groupToNum(e, tips));
 
 //   // Loop through group possibilities and append to fits
 //   let fits: LocalClockModel[] = [];
@@ -315,18 +316,14 @@ export function BIC(regs: Regression[]): number {
     return (3 * f * Math.log(n) - (2 * totLogLik)); 
 }
 
-/* COmmenting out clock search for now 
-function getGroups (tree: any, minCladeSize: number, maxNumClocks: number): Array<Array<number>> { // TODO: Review type declarations here
-  // NB. Tips from tree are in identical order to that extected by regression. 
-  // Conferred with L38 @ ../tree/treeSlice.ts/
-
-  // Clades descending from each node
+export function getGroups (tree: any, minCladeSize: number, numClocks: number): string[][][] {
+  // Sets of tips descending from each node
   var tips: any = [];
+  // list tips descending from each node
   tree.nodes.each((node: any) => tips.push(node.leaves())); 
+  // map to just tip names
   var tips = tips.map((e0: any) => e0.map((e1: any) => e1.data.name));
-
-  // De-duplicate clades (artefact from d3 hierarchy)
-  // Have verified that length of output = num internal nodes
+  // De-duplicate tips (artefact from d3 hierarchy)
   var uniqueTips = tips.map(
     (e: string[]) => JSON.stringify(e)
     ).filter(
@@ -334,29 +331,51 @@ function getGroups (tree: any, minCladeSize: number, maxNumClocks: number): Arra
       ).map(
        (e: string) => JSON.parse(e)
         )
-
   // Sort clades based on size. Largest (all tips) goes first
   var sortedUniqueTips = uniqueTips.sort((a: string[], b: string[]) => {return b.length - a.length});
 
-  // Filter out all clades below certain ${minCladeSize} in size;
+  // Filter out all clades with fewer than ${minCladeSize} tips
   var finalClades = sortedUniqueTips.filter((e: string[]) => e.length >= minCladeSize);
 
-  // Now generate groups
-  let comb = combn(Array.from(Array(finalClades.length).keys()).map((e: number, i: number ) => i+1), (maxNumClocks - 1));
+  // array from 1:(finalClades.length) possible clades to draw combinations
+  var grpNums = Array.from(Array(finalClades.length).keys());
+  // +1 to account for 0th base clock
+  //var grpNums = grpNums.map((e: number, i: number ) => i+1); 
+  
+  // remove 0th group - that being all tips. Add back in later
+  grpNums.shift()
+  // get all combinations of groups to make up ${numClocks} local clocks
+  let comb = combn(grpNums, (numClocks - 1))
   // add 0th clade for background rate
-  // Issue us here adding zero!. the .unshift method modified combn in place. 
-  //Assigning to a new variable only assigns lengths
   comb.map((e: number[]) => e.unshift(0)); 
 
-  // Convert number combinations to clade combinations
+  // Convert number combinations to corresponding groups of tips
   let allGroups: string[][][] = [];
   for (let i = 0; i < comb.length; i++){
-    var tmp = comb[i];
-    allGroups[i] = tmp.map((e: number) => finalClades[e]);
+    allGroups.push(
+      comb[i].map(
+        (e: number) => finalClades[e]
+      )
+    );
   }
 
-  let allGroupsNumber = allGroups.map((e: string[][]) => groupToNum(e, tips));
-  return allGroupsNumber;
+  //return groups after converting to non-intersecting groups;
+  return allGroups.map(e => getUnique(e));
+
+  // TODO: Size-filtering and defend against requesting too many clocks
+}
+
+// function takes string[][] and maps each to unique values assuming each string[] is nested as for allGroups
+export function getUnique(x: string[][]): string[][] {
+  return x.map((e0: string[], i) => e0.filter(
+      (e1: string) => {
+        if(i+1 < x.length) { 
+          return !x[i+1].includes(e1); 
+        } else {
+          return true;
+        }
+      })
+  );
 }
 
 // A function that takes a list of tips and returns group number based on an element of allGroups
@@ -365,6 +384,7 @@ function groupToNum(arr: string[][], tips: string[]): number[] {
 
     for (let i = 0; i < tips.length; i++){
       var tmp = [];
+      
       for (let j = 0; j < arr.length; j++) {
         if (arr[j].indexOf(tips[i]) > -1) {
           groupings[i] = j;
@@ -372,11 +392,11 @@ function groupToNum(arr: string[][], tips: string[]): number[] {
       }
     }
   return groupings;
-} // TODO: Include a test here to check that output is all integers */
+} // TODO: Include a test here to check that output is all integers and that ordering of tips matches input
 
 
 // generating combinations of groups
-function combn(arr: any[], k: number): number[][] {
+ export function combn(arr: any[], k: number): number[][] {
   // Store all possible combinations in a result array
   const result: number[][] = [];
 
