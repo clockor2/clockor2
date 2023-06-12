fdef19a75dc93882bc33393c8f3e2f576bc80e9c
Wytamma Wirth <wytamma.wirth@me.com>
2023-06-12 15:59:53 +1000

    :sparkles: service-worker for offline use

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
 // https://alchemy.com/blog/how-to-polyfill-node-core-modules-in-webpack-5
 const webpack = require('webpack');
+const WorkBoxPlugin = require('workbox-webpack-plugin');
+
 module.exports = function override(config) {
     const fallback = config.resolve.fallback || {};
     Object.assign(fallback, {
@@ -12,6 +14,11 @@ module.exports = function override(config) {
         "url": require.resolve("url")
     })
     config.resolve.fallback = fallback;
+    config.plugins.forEach(plugin => {
+        if ( plugin instanceof WorkBoxPlugin.InjectManifest) {
+          plugin.config.maximumFileSizeToCacheInBytes = 50*1024*1024;
+        }
+      });
     config.plugins = (config.plugins || []).concat([
         new webpack.ProvidePlugin({
             process: 'process/browser',
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
@@ -14,6 +15,11 @@ root.render(
       <App />
     </Provider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
