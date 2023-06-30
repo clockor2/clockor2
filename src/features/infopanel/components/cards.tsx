8b0ae96975ff372916d24214f713ebb031643a46
LeoFeatherstone <leofeatherstone@gmail.com>
2023-06-30 15:57:56 +1000

    remove Card component

--- src/features/infopanel/components/cards.tsx
+++ src/features/infopanel/components/cards.tsx
@@ -8,11 +8,11 @@ interface Props {
 
 export function MetricCard(props: Props){
     return (
-        <Card className={props.isMin ? "border-2 border-blue-400" : "border-2 border-white"}>
+        <div className={props.isMin ? "border-4 border-green-400 p-6 rounded-md" : "border-4 border-gray-300 p-6 rounded-md"}>
             <div className="flex flex-row space-x-1">
-                <div className="text-base text-grey-900">{ props.text }</div>
+                <div className="text-base text-gray-700">{ props.text }</div>
                 <div className="text font-bold ">{ props.value ? props.value.toFixed(2) : "Undefined"}</div>
             </div> 
-        </Card>
+        </div>
     )
 }
