d6c2681d01560c5ab46dd6239e45733e33ac3f95
LeoFeatherstone <leofeatherstone@gmail.com>
2023-07-01 10:48:20 +1000

    :bug: IC card highlighting

--- src/features/infopanel/components/cards.tsx
+++ src/features/infopanel/components/cards.tsx
@@ -1,14 +1,16 @@
-import { Card } from "flowbite-react";
-
 interface Props {
     text: string;
     value: number | undefined | null;
     isMin: boolean;
 }
 
+let cardClassName = "block max-w-sm p-3 border-2 rounded-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700";
+
 export function MetricCard(props: Props){
+    let style = cardClassName + (props.isMin ? " bg-green-400 bg" : "bg-inherit");
     return (
-        <div className={props.isMin ? "border-4 border-green-400 p-6 rounded-md" : "border-4 border-gray-300 p-6 rounded-md"}>
+        
+        <div className={style}>
             <div className="flex flex-row space-x-1">
                 <div className="text-base text-gray-700">{ props.text }</div>
                 <div className="text font-bold ">{ props.value ? props.value.toFixed(2) : "Undefined"}</div>
