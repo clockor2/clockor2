8051443b21667bbffcfb08761f09f426cfbe614b
Wytamma Wirth <wytamma.wirth@me.com>
2023-11-21 17:53:23 +1100

    :bug: fix background on mobile

--- src/features/utils/darkmode.tsx
+++ src/features/utils/darkmode.tsx
@@ -32,8 +32,10 @@ export const useDarkMode = () => {
         // Set the initial mode based on localStorage
         if (isDarkMode) {
             document.documentElement.classList.add('dark');
+            document.documentElement.classList.add('bg-slate-900');
         } else {
             document.documentElement.classList.remove('dark');
+            document.documentElement.classList.remove('bg-slate-900');
         }
 
         // Cleanup
